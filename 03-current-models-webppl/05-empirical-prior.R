2e94c57e74084b95d424fee7ae966501a389615f
cbergey <cbergey@hawkins-gpu-1.discovery.wisc.edu>
2024-07-01 01:23:29 -0500

    cs2 preds
    Former-commit-id: e8f722351f9dd06ba7c5f16103b1a7d709bb37bb

--- 03-current-models-webppl/05-empirical-prior.R
+++ 03-current-models-webppl/05-empirical-prior.R
@@ -68,12 +68,12 @@ run_model_tso <- function (params, utils) {
 
 priorSampleParams <- function() {
   params <- tibble(
-    'policyAlpha'      = runif(1,min = 0, max = 10), # searched 0-10
-    'questionerAlpha'  = runif(1,min = 0, max = 10), # searched 0-10
-    'R1Alpha'          = runif(1,min = 0, max = 10), # searched 0-10
+    'policyAlpha'      = runif(1,min = 1, max = 2), # searched 0-10
+    'questionerAlpha'  = runif(1,min = 4, max = 5), # searched 0-10
+    'R1Alpha'          = runif(1,min = 3, max = 4), # searched 0-10
     'relevanceBetaR0'  = runif(1,min = 0, max = 0), # fixed at 0
-    'relevanceBetaR1'  = runif(1,min = 0, max = 1), # searched 0-1
-    'costWeight'       = runif(1,min = 0, max = 5), # searched 0-5
+    'relevanceBetaR1'  = runif(1,min = 0, max = 0.1), # searched 0-1
+    'costWeight'       = runif(1,min = 2, max = 2.5), # searched 0-5
     'questionCost'     = runif(1,min = 0, max = 0) # fixed at 0
   )
   return(params)
@@ -113,5 +113,5 @@ priorPred <- furrr::future_map_dfr(1:n_samples, function(i) {
   return (out)
 }, .progress = TRUE, .options = furrr_options(seed = 123))
 
-write_csv(priorPred, './03-current-models-webppl/data/case_study_2_parameter_search.csv')
-#write_csv(priorPred, './03-current-models-webppl/data/case_study_2_RSA_preds.csv')
+#write_csv(priorPred, './03-current-models-webppl/data/case_study_2_parameter_search.csv')
+write_csv(priorPred, './03-current-models-webppl/data/case_study_2_RSA_preds.csv')
